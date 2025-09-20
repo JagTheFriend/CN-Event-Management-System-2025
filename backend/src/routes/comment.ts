1bcd56d9c8e7b9d400336c91453b17453a86a7eb
JagTheFriend <JagTheFriend12@gmail.com>
2025-09-20 20:41:17 +0530

    Added backend routes
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ backend/src/routes/comment.ts
@@ -0,0 +1,60 @@
+import { Router } from "express";
+import { db } from "@/db";
+
+export const commentRouter = Router();
+
+commentRouter.get("/event/:eventId", async (req, res) => {
+	const { eventId } = req.params;
+	const data = await db.comment.findMany({
+		where: {
+			eventId,
+		},
+	});
+	return res.json(data);
+});
+
+commentRouter.get("/user/:userId", async (req, res) => {
+	const { userId } = req.params;
+	const data = await db.comment.findMany({
+		where: {
+			userId,
+		},
+	});
+	return res.json(data);
+});
+
+commentRouter.post("/new", async (req, res) => {
+	const { content, eventId, userId } = req.body;
+	const data = await db.comment.create({
+		data: {
+			content,
+			eventId,
+			userId,
+		},
+	});
+	return res.json(data);
+});
+
+commentRouter.put("/edit/:id", async (req, res) => {
+	const { id } = req.params;
+	const { content } = req.body;
+	const data = await db.comment.update({
+		where: {
+			id,
+		},
+		data: {
+			content,
+		},
+	});
+	return res.json(data);
+});
+
+commentRouter.delete("/delete/:id", async (req, res) => {
+	const { id } = req.params;
+	const data = await db.comment.delete({
+		where: {
+			id,
+		},
+	});
+	return res.json(data);
+});
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,12 @@ import cors from "cors";
 import { config as dotenv } from "dotenv";
 import express, {
 	type Application,
+	type NextFunction,
 	type Request,
 	type Response,
 } from "express";
+import { commentRouter } from "./routes/comment";
+import { eventRouter } from "./routes/event";
 
 dotenv();
 
@@ -12,8 +15,17 @@ const app: Application = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+app.use("/event", eventRouter);
+app.use("/comment", commentRouter);
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+	console.error(err.stack);
+	res.status(500).json({ data: null, message: "Internal Server Error" });
+});
+
+app.get("/", (_req: Request, res: Response) => {
 	res.send("Hello World");
 });
 
