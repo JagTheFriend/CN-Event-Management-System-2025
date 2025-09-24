23ef24acbf0ad7fb4bd34e6303ba5a828df537e3
PRATIK <pratikh004@gmail.com>
2025-09-25 00:20:08 +0530

    event page logic added

--- frontend/src/pages/Events.tsx
+++ frontend/src/pages/Events.tsx
@@ -3,23 +3,43 @@ import { Alert, AlertTitle } from "@/components/ui/alert";
 import { Input } from "@/components/ui/input";
 import events from "@/data/events.json"; // TODO: remove dummy data
 import type { Event } from "@/interfaces/event.interface";
-import { useState } from "react";
+import { useState, type JSX } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "@/lib/config";
 
-export default function Events() {
+export default async function Events() {
   // @ts-ignore
-  const [filteredEvents, setFilteredEvents] = useState<Event[]>(events);
-  const [searchText, setSearchText] = useState('');
+  const [filteredEvents, setFilteredEvents] = useState<Event[]>([]);
+  const [searchText, setSearchText] = useState("");
 
   const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const query = e.currentTarget.value;
     setSearchText(query);
     setFilteredEvents(
       // @ts-ignore
-      searchText.length > 0 ? events.filter(event =>
-        event.title.toLowerCase().includes(query.toLowerCase())
-      ) : events
+      searchText.length > 0
+        ? events.filter((event) =>
+            event.title.toLowerCase().includes(query.toLowerCase())
+          )
+        : events
     );
   };
+
+  type EventsApiResponse = {
+    success: boolean;
+    data: Event[];
+    length: number;
+    map: (
+      arg0: (item: Event, index: number) => JSX.Element
+    ) => import("react").ReactNode;
+  };
+
+  const { data: AllEvents } = await axios.get<EventsApiResponse>(
+    `${BACKEND_URL}/event`
+  );
+  if (AllEvents.success) {
+    setFilteredEvents(AllEvents.data);
+  }
 
   return (
     <>
@@ -30,18 +50,20 @@ export default function Events() {
           onChange={handleSearchInput}
         />
       </div>
-      {filteredEvents.length > 0 && <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 space-y-6">
-        {filteredEvents.map((item, index) => (
-          <EventCard key={index} {...item} />
-        ))}
-
-
-      </div>}
-      {filteredEvents.length === 0 && <div className="px-4">
-        <Alert variant="destructive">
-          <AlertTitle>No search results found for "{searchText}"</AlertTitle>
-        </Alert>
-      </div>}
+      {AllEvents.length > 0 && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 space-y-6">
+          {AllEvents.map((item, index) => (
+            <EventCard key={index} {...item} />
+          ))}
+        </div>
+      )}
+      {AllEvents.length === 0 && (
+        <div className="px-4">
+          <Alert variant="destructive">
+            <AlertTitle>No search results found for "{searchText}"</AlertTitle>
+          </Alert>
+        </div>
+      )}
     </>
   );
 }
