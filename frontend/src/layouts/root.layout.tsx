6e8c605d3ad24c6b85192ad8ba936d2788508033
JagTheFriend <JagTheFriend12@gmail.com>
2025-10-26 20:43:24 +0530

    Added more padding
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- frontend/src/layouts/root.layout.tsx
+++ frontend/src/layouts/root.layout.tsx
@@ -3,14 +3,13 @@ import { ToastContainer } from "react-toastify";
 import Navbar from "@/components/Navbar";
 
 export default function RootLayout() {
-    return (
-        <>
-            <Navbar />
-            <ToastContainer />
-
-            <div className="pt-20">
-                <Outlet />
-            </div>
-        </>
-    )
+	return (
+		<>
+			<Navbar />
+			<ToastContainer />
+			<div className="pt-24">
+				<Outlet />
+			</div>
+		</>
+	);
 }
